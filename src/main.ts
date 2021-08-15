82aa870a9693fa4d2b1ea070620a5a8548f460d6
leap0229 <tomoya0229@gmail.com>
2021-08-15 15:40:21 +0900

    config:add globalprefix /api

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
 
